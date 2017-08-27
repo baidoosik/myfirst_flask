c0d624ec7831c9a7d97bc76e5830386cc2634020
baidoosik <qoentlr37@naver.com>
2017-08-27 15:23:22 +0900

    flask-script 확장 연결

--- hello.py
+++ hello.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from flask_script import Manager
 
 app= Flask(__name__)
+# flask script runserver, shell 실행 확장
+manager = Manager(app)
 
 @app.route('/')
 def index():
-    return '<h1>hello flask world'
+    return '<h1>hello flask world</h1>'
+
+@app.route('/profile/<int:id>/')
+def profile():
+    return '<h1>profile</h1>'
 
 
-if __name__ =='__main__'
+if __name__ =='__main__':
+    manager.run()
