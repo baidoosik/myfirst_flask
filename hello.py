b5ed2ef4b22f25aaf3a460121a4bd4810b85afc1
baidoosik <qoentlr37@naver.com>
2017-08-27 16:08:08 +0900

    flask bootstrap 설치하고 연동

--- hello.py
+++ hello.py
@@ -1,18 +1,32 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_wtf import form
 
 app= Flask(__name__)
 # flask script runserver, shell 실행 확장
 manager = Manager(app)
+#flask-bootstrap 적용
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return '<h1>hello flask world</h1>'
+    return render_template('index.html')
 
-@app.route('/profile/<int:id>/')
+@app.route('/profile/')
 def profile():
-    return '<h1>profile</h1>'
+    #user_id =profile_id
+    return render_template('profile.html')
 
+#404, 500 page
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'),500
 
 if __name__ =='__main__':
     manager.run()
