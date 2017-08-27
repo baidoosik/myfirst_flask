848a17abd5a1251395e570d58abec6de222e9ce3
baidoosik <qoentlr37@naver.com>
2017-08-27 17:02:32 +0900

    flask form flaks-wtf연결하고, 뷰함수연결

--- hello.py
+++ hello.py
@@ -1,18 +1,37 @@
-from flask import Flask,render_template
+import json
+from flask import Flask,render_template,redirect,session,url_for
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
-from flask_wtf import form
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import Email,Required
+
+
+with open('envs.json','r') as f:
+    envs = json.loads(f.read())
 
 app= Flask(__name__)
+app.config['SECRET_KEY']=envs['csrf_key']
 # flask script runserver, shell 실행 확장
 manager = Manager(app)
 #flask-bootstrap 적용
 bootstrap = Bootstrap(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+class NameForm(FlaskForm):
+    name= StringField('what is your naem?',validators=[Required()])
+    email = StringField('what is your e-mail?',validators=[Required(),Email()])
+    submit =SubmitField('Submit')
 
+@app.route('/',methods=['GET','POST'])
+def index():
+    form =NameForm()
+
+    if form.validate_on_submit():
+        session['name']= form.name.data
+        session['email'] = form.email.data
+        return redirect(url_for('index')) #post/rediret/get patter 기법. 마지막 요청을 post로 남기지 않기 위해.
+
+    return render_template('index.html',form=form,name=session['name'])
 @app.route('/profile/')
 def profile():
     #user_id =profile_id
