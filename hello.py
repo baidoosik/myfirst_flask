11c2103d40777533c581bbcf6df41faca6217787
baidoosik <qoentlr37@naver.com>
2017-08-28 00:29:07 +0900

    flask-sqlalchemy 연동 및 sqlite 연결,폼을 통해 입력받은 데이터 db에 연결

--- hello.py
+++ hello.py
@@ -1,24 +1,56 @@
+import os
 import json
 from flask import Flask,render_template,redirect,session,url_for,flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
+from flask_sqlalchemy import SQLAlchemy
 from wtforms import StringField,SubmitField
 from wtforms.validators import Email,Required
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 with open('envs.json','r') as f:
     envs = json.loads(f.read())
 
 app= Flask(__name__)
 app.config['SECRET_KEY']=envs['csrf_key']
+app.config['SQLALCHEMY_DATABASE_URI']=\
+    'sqlite:///'+ os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =True
+
+# flask app과 db 연결
+db = SQLAlchemy(app)
 # flask script runserver, shell 실행 확장
 manager = Manager(app)
 #flask-bootstrap 적용
 bootstrap = Bootstrap(app)
 
+
+# model 정의
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id =  db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(64),unique=True)
+    users = db.relationship('User',backref='role')
+
+    def __repr__(self):
+        return '<Role {}>'.format(self.name)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id= db.Column(db.Integer,primary_key=True)
+    user_name = db.Column(db.String(40),unique=True,index=True)
+    email = db.Column(db.String(60))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<USER {}>'.format(self.user_name)
+
+# form 정의
 class NameForm(FlaskForm):
-    name= StringField('what is your naem?',validators=[Required()])
+    name= StringField('what is your name?',validators=[Required()])
     email = StringField('what is your e-mail?',validators=[Required(),Email()])
     submit =SubmitField('Submit')
 
@@ -27,16 +59,26 @@ def index():
     form =NameForm()
 
     if form.validate_on_submit():
-        old_name = session['name']
+        name = User.query.filter_by(user_name=form.name.data).first()
 
-        if old_name is not None and old_name !=form.name.data:
-            flash('사용자가 변경되었습니다.')
+        if name is None:
+            #새로운 사용자 db에 추가
+            flash('db에 사용자가 추가되었습니다.')
+            user = User(user_name=form.name.data,email=form.email.data)
+            db.session.add(user)
+            session['known']=False
+        else:
+            session['known']=True
+
         session['name']= form.name.data
         session['email'] = form.email.data
+        form.name.data=''
+        form.email.data=''
 
         return redirect(url_for('index')) #post/rediret/get patter 기법. 마지막 요청을 post로 남기지 않기 위해.
 
-    return render_template('index.html',form=form,name=session['name'])
+    return render_template('index.html',form=form,name=session['name'],
+                           known=session.get('known',False))
 @app.route('/profile/')
 def profile():
     #user_id =profile_id
